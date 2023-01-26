514ad02fa265f42f7bcafbc7814c69a346e6f955
Kevin Thomas <kevin.jt2007@gmail.com>
2023-01-25 16:58:57 -0800

    feat: finished j3 2020

---
+++ 2020/j3.py
@@ -0,0 +1,9 @@
+# Art -
+
+num_of_drops = int(input())
+drops = [tuple(map(int, input().split(","))) for _ in range(num_of_drops)]
+x_values = [drop[0] for drop in drops]
+y_values = [drop[1] for drop in drops]
+
+print(f"{min(x_values) - 1},{min(y_values) - 1}")
+print(f"{max(x_values) + 1},{max(y_values) + 1}")
