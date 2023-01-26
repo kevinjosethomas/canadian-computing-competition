7e76aff557ba230fd15117e6affc5798624fc51f
Kevin Thomas <kevin.jt2007@gmail.com>
2023-01-25 16:39:49 -0800

    feat: finished j1 2020

---
+++ 2020/j1.py
@@ -0,0 +1,11 @@
+# Dog Treats - 15/15
+
+small = int(input())
+medium = 2 * int(input())
+large = 3 * int(input())
+total = small + medium + large
+
+if total >= 10:
+    print("happy")
+else:
+    print("sad")
