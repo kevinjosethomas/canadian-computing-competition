e5e2eda485e70664dedc2de74097022282d57fa3
Kevin Thomas <kevin.jt2007@gmail.com>
2023-02-15 09:26:27 -0800

    feat: one lined j2 cuz im a boss

--- 2023/j2.py
+++ 2023/j2.py
@@ -1,6 +1,3 @@
 # Chili Peppers
 
-PEPPERS = {"Poblano": 1500, "Mirasol": 6000, "Serrano": 15500, "Cayenne": 40000, "Thai": 75000, "Habanero": 125000}
-
-total = sum([PEPPERS[input()] for _ in range(int(input()))])
-print(total)
+print(sum([{"Poblano": 1500, "Mirasol": 6000, "Serrano": 15500, "Cayenne": 40000, "Thai": 75000, "Habanero": 125000}[input()] for _ in range(int(input()))]))
